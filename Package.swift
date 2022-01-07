ea44332c3f05ba6d16e6f50fde879d5e8e00a6eb
Shagun Madhikarmi <madhikarma@gmail.com>
2022-01-07 21:43:10 +0000

    Fix SwiftPM

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "BDKCollectionIndexView",
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -20,7 +21,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BDKCollectionIndexView",
-            dependencies: [],
-            publicHeadersPath: "Sources")
+            publicHeadersPath: "Sources/BDKCollectionIndexView")
     ]
 )
